85f5c0cc823d317ecc1b40768bdd095c6fc304e8
Soumyajeet Satpathy <satpathysoumyajeet.99@gmail.com>
2023-11-25 16:33:10 +0530

    added hire me component

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Link from 'next/link'
 import profilePic from '../../public/images/profile/developer-pic-1.png'
 import AnimatedText from '@/components/AnimatedText'
 import { LinkArrow } from '@/components/icons'
+import HireMe from '@/components/HireMe'
+import lightbulb from '../../public/images/svgs/miscellaneous_icons_1.svg'
 
 export default function Home() {
   return (
@@ -20,23 +22,27 @@ export default function Home() {
               <Image src={profilePic} alt='Soumyajeet Satpathy' className='w-full h-auto' />
             </div>
             <div className='w-1/2 flex flex-col items-center self-center'>
-              <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6xl !text-left'/>
+              <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6xl !text-left' />
               <p className='my-4 text-base font-medium'>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
                 Explore my latest projects and articles, showcasing my expertise in React.js and web development.</p>
-                <div className='flex items-center self-start mt-2'>
-                  <Link href="/dummy.pdf" target={"_blank"}
+              <div className='flex items-center self-start mt-2'>
+                <Link href="/dummy.pdf" target={"_blank"}
                   className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark'
                   download={true}
-                  >Resume <LinkArrow className={"w-6 ml-1"}/>
-                  
-                  </Link>
-                  <Link href="mailto:satpathysoumyajeet.99@gmail.com" target={"_blank"}
+                >Resume <LinkArrow className={"w-6 ml-1"} />
+
+                </Link>
+                <Link href="mailto:satpathysoumyajeet.99@gmail.com" target={"_blank"}
                   className='ml-4 text-lg font-medium capitalize text-dark underline'
-                  >Contact</Link>
-                </div>
+                >Contact</Link>
+              </div>
             </div>
           </div>
         </Layout>
+        <HireMe />
+        <div className='absolute right-8 bottom-8 inline-block w-24'>
+          <Image src={lightbulb} alt='Light Bulb Soumyajeet' className='w-full h-auto' />
+        </div>
       </main>
     </>
   )
