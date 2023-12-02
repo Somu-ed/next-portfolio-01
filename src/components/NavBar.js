2a140da5ac5e389cf4ba4ddde4311d18531c6454
Soumyajeet Satpathy <satpathysoumyajeet.99@gmail.com>
2023-12-02 16:49:40 +0530

    adding dark mode

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,10 @@
 import Link from 'next/link'
 import React from 'react'
 import Logo from '@/components/Logo'
-import { TwitterIcon, DribbbleIcon, GithubIcon, LinkedInIcon, PinterestIcon } from '@/components/icons'
+import { TwitterIcon, DribbbleIcon, GithubIcon, LinkedInIcon, PinterestIcon, SunIcon, MoonIcon } from '@/components/icons'
 import { useRouter } from 'next/router'
 import { motion } from 'framer-motion'
+import useThemeSwitcher from './hooks/useThemeSwitcher'
 
 const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
@@ -14,21 +15,24 @@ const CustomLink = ({ href, title, className = "" }) => {
 
       <span className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5
       group-hover:w-full transition-[width] ease duration-300
-      ${router.asPath === href ? 'w-full' : 'w-0'}
+      ${router.asPath === href ? 'w-full' : 'w-0'} dark:bg-light
       `}>&nbsp;</span>
     </Link>)
 }
 
 const NavBar = () => {
+
+  const [mode, setMode] = useThemeSwitcher();
+
   return (
     <header
-      className='w-full px-32 py-8 font-medium flex items-center justify-between'
+      className='w-full px-32 py-8 font-medium flex items-center justify-between dark:text-light'
     >
       <nav>
         <CustomLink href='/' title="Home" className='mr-4' />
         <CustomLink href='/about' title="About" className='mx-4' />
-        <CustomLink href='/projects' title="Projects" className='mx-4' />
-        <CustomLink href='/articles' title="Articles" className='ml-4' />
+        {/* <CustomLink href='/projects' title="Projects" className='mx-4' /> */}
+        {/* <CustomLink href='/articles' title="Articles" className='ml-4' /> */}
       </nav>
 
       <nav className='flex items-center justify-center flex-wrap'>
@@ -67,6 +71,20 @@ const NavBar = () => {
         >
           <DribbbleIcon />
         </motion.a>
+
+        <button
+        onClick={() => setMode(mode === "light" ? "dark" : "light")}
+        className={`ml-3 flex items-center justify-center rounded-full p-1
+        ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
+        `}
+        >
+          {
+            mode === "dark" ?
+            <SunIcon className={"fill-dark"}/>
+            : <MoonIcon className={"fill-dark"}/>
+          }
+        </button>
+
       </nav>
 
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
