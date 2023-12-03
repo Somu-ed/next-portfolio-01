8964c8122df187d7e3e8736f4d99887a16682358
Soumyajeet Satpathy <satpathysoumyajeet.99@gmail.com>
2023-12-03 10:30:54 +0530

    img optimization

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,22 +9,22 @@ import Skills from '@/components/Skills'
 import Experience from '@/components/Experience'
 import Education from '@/components/Education'
 
-const AnimatedNumbers = ({value}) => {
+const AnimatedNumbers = ({ value }) => {
   const ref = useRef(null);
 
   const motionValue = useMotionValue(0);
   const springValue = useSpring(motionValue, { duration: 3000 });
-  const isInView = useInView(ref, {once: true});
+  const isInView = useInView(ref, { once: true });
 
   useEffect(() => {
-    if(isInView){
+    if (isInView) {
       motionValue.set(value);
     }
   }, [isInView, value, motionValue]);
 
   useEffect(() => {
     springValue.on("change", (latest) => {
-      if(ref.current && latest.toFixed(0) <= value){
+      if (ref.current && latest.toFixed(0) <= value) {
         ref.current.textContent = latest.toFixed(0);
       }
     })
@@ -63,24 +63,29 @@ const about = () => {
             </div>
             <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light'>
               <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light' />
-              <Image src={profilepic} alt="Soumyajeet" className='w-full h-auto rounded-2xl' />
+              <Image src={profilepic} alt="Soumyajeet" className='w-full h-auto rounded-2xl'
+                priority
+                sizes='(max-width: 768px) 100vw,
+                  (max-width: 1200px) 50vw,
+                  33vw'
+              />
             </div>
             <div className='col-span-2 flex flex-col items-end justify-between'>
               <div className='flex flex-col items-end justify-center'>
                 <span className='inline-block text-7xl font-bold'>
-                  <AnimatedNumbers value={50}/>+
+                  <AnimatedNumbers value={50} />+
                 </span>
                 <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>satisfied clients</h2>
               </div>
               <div className='flex flex-col items-end justify-center'>
                 <span className='inline-block text-7xl font-bold'>
-                <AnimatedNumbers value={40}/>+
+                  <AnimatedNumbers value={40} />+
                 </span>
                 <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>projects completed</h2>
               </div>
               <div className='flex flex-col items-end justify-center'>
                 <span className='inline-block text-7xl font-bold'>
-                <AnimatedNumbers value={4}/>+
+                  <AnimatedNumbers value={4} />+
                 </span>
                 <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>years of experience</h2>
               </div>
